d16772201e78dd93d7dc978cf0a394a5a829f968
Dan Ross <daniellross@gmail.com>
2021-06-17 18:25:48 -0500

    coffee detail loads and displays properly

--- src/components/coffee/CoffeeDetail.js
+++ src/components/coffee/CoffeeDetail.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { CoffeeContext } from "./CoffeeProvider"
 
 export const CoffeeDetail = () => {
@@ -16,15 +16,18 @@ export const CoffeeDetail = () => {
         <>
             {
                 coffee.id ?
-                <div>
-                    <h2>{coffee.roaster} {coffee.name}</h2>
-                    <p>Country: {coffee.country}</p>
-                    <p>Region: {coffee.region}</p>
-                    <p>Recommended Brew Method: {coffee.recommended_method}</p>
-                    <p>Tasting Notes: {coffee.tasting_notes}</p>
-                    <p>Tasting Notes: {coffee.tasting_notes}</p>
-                </div>
-                : <div></div>
+                    <div>
+                        <h2>{coffee.roaster} {coffee.name}</h2>
+                        <a href={coffee.website}>
+                            <img src={coffee.coffee_image}></img>
+                        </a>
+                        <p>Country: {coffee.country}</p>
+                        <p>Region: {coffee.region}</p>
+                        <p>Recommended Brew Method: {coffee.recommended_method}</p>
+                        <p>Tasting Notes: {coffee.tasting_notes}</p>
+                        <p>Process: {coffee.process}</p>
+                    </div>
+                    : <div></div>
             }
         </>
     )
