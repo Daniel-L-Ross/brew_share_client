b5fb6812a4cd6dc1e86d9848eadad3d8205e5d2c
Dan Ross <daniellross@gmail.com>
2021-06-17 15:53:20 -0500

    added fetch call to get single entry. created entry detail page and route. Entry details and all entry steps render

---
+++ src/components/entries/EntryDetail.js
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import { EntryContext } from "./EntryProvider"
+
+export const EntryDetail = () => {
+    const { getSingleEntry } = useContext(EntryContext)
+    const [entry, setEntry] = useState({})
+    const { entryId } = useParams()
+
+
+    useEffect(() => {
+        getSingleEntry(entryId)
+            .then(setEntry)
+    }, [])
+
+
+    return (
+        <>
+            {
+                entry.id ?
+                    <div>
+                        {/* TODO: add links to brew method and coffee */}
+                        <h2> {entry.brewer.user.first_name}'s {entry.title}</h2>
+                        <p>Notes: {entry.review}</p>
+                        <p>Rating: {entry.rating}/5</p>
+                        <p>Tasting-notes: {entry.tasting_notes}</p>
+                        <p>Brewing: {entry.coffee.roaster} {entry.coffee.name}</p>
+                        <p>Grind size: {entry.grind_size}</p>
+                        <p>Method: {entry.method.name}</p>
+                        <p>Water: {entry.water_volume}g at {entry.water_temp} F</p>
+                        <h3>Setup</h3>
+                        <p>{entry.setup}</p>
+                        {
+                            (entry.steps.length > 1) ?
+                                <div>
+                                    <h2>STEPS</h2>
+                                    <div>
+                                        {entry.steps.map(step => {
+                                            return <div key={`step--${step.id}`}>
+                                                <p>{step.seconds}</p>
+                                                <p>{step.descriptor}</p>
+                                                <p>{step.instruction}</p>
+                                                {/* TODO: add images of each step if present */}
+                                            </div>
+                                        })}
+                                    </div>
+                                </div>
+                                : <div></div>
+                        }
+                    </div>
+                    : <div>loading...</div>
+            }
+        </>
+    )
+}
--- src/components/entries/EntryList.js
+++ src/components/entries/EntryList.js
@@ -14,8 +14,11 @@ export const EntryList = () => {
             <h2>Entry List</h2>
             {
                 entries.map(entry => {
-                    return <div>
+                    return <div key={`entry--${entry.id}`}>
+                        <Link to={`/entries/${entry.id}`}>
                         <h3>{entry.title} | {entry.brewer.user.first_name} {entry.brewer.user.last_name}</h3>
+                        </Link>
+
                         <p>{entry.coffee.roaster} {entry.coffee.name} - {entry.method.name}</p>
                     </div>
                 })
