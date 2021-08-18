253c72a98d50d9fb80ade21db5924246b2e2dcdf
Dan Ross <daniellross@gmail.com>
2021-08-18 14:28:19 -0500

    decided to go with bootstrap

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { username } from "../auth/authSettings"
 import "./NavBar.css"
@@ -10,54 +11,71 @@ const handleLogout = () => {
 export const NavBar = () => {
 
     return (
-        <nav className="navbar sticky">
-            <div className="navbar__item">
-                <Link to="/">
-                    brew_share
-                </Link>
-            </div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    <div className="navbar__item">
+                        <Link to="/">
+                            brew_share
+                        </Link>
+                    </div>
+                </Navbar.Brand>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link>
 
-            <div className="navbar__item">
-                <Link to="/">
-                    Entries
-                </Link>
-            </div>
+                            <div className="navbar__item">
+                                <Link to="/">
+                                    Entries
+                                </Link>
+                            </div>
+                        </Nav.Link>
 
-            <div className="navbar__item">
-                <Link to="/entries/create">
-                    Add Entry
-                </Link>
-            </div>
+                        <Nav.Link>
+                            <div className="navbar__item">
+                                <Link to="/entries/create">
+                                    Add Entry
+                                </Link>
+                            </div>
+                        </Nav.Link>
+                        <NavDropdown title="More" id="basic-nav-dropdown">
 
-            <div className="navbar__item">
-                <Link to={`/${username()}/my-entries`}>
-                    My Entries
-                </Link>
-            </div>
+                            <NavDropdown.Item>
+                                <div className="navbar__item">
+                                    <Link to={`/${username()}/my-entries`}>
+                                        My Entries
+                                    </Link>
+                                </div>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item>
+                                <div className="navbar__item">
+                                    <Link to={`/${username()}/favorites`}>
+                                        My Favorites
+                                    </Link>
+                                </div>
+                            </NavDropdown.Item>
 
-            <div className="navbar__item">
-                <Link to={`/${username()}/favorites`}>
-                    My Favorites
-                </Link>
-            </div>
+                            <div className="navbar__item">
+                                <Link to="/coffee">
+                                    Coffee
+                                </Link>
+                            </div>
 
-            <div className="navbar__item">
-                <Link to="/coffee">
-                    Coffee
-                </Link>
-            </div>
+                            <div className="navbar__item">
+                                <Link to="/brew-methods">
+                                    Brew Methods
+                                </Link>
+                            </div>
 
-            <div className="navbar__item">
-                <Link to="/brew-methods">
-                    Brew Methods
-                </Link>
-            </div>
-
-            <div className="navbar__item" >
-                <a href="/" onClick={handleLogout}>
-                    Logout
-                </a>
-            </div>
-        </nav>
+                            <div className="navbar__item" >
+                                <a href="/" onClick={handleLogout}>
+                                    Logout
+                                </a>
+                            </div>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import { BrewShare } from './components/BrewShare';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
